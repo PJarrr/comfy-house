6579fed6dac2e22e24e793c1038b383621924916
Paulius Jarutis <76951786+PJarrr@users.noreply.github.com>
2021-03-17 14:03:40 +0200

    cart : remove item, increase/decrease amount

--- app.js
+++ app.js
@@ -134,7 +134,7 @@ getCartButtons() {
                     </div>
                     <div>
                         <i class="fas fa-chevron-up" data-id=${item.id}></i>
-                        <p class="item-amount" data-id=${item.amount}></p>
+                        <p class="item-amount" data-id=${item.id}>${item.amount}</p>
                         <i class="fas fa-chevron-down"data-id=${item.id}></i>
                     </div>`
 
@@ -142,18 +142,102 @@ getCartButtons() {
     }
 
    showCart(){
-       cartOverlay.classList.add("transparentBcg")
-       cartDOM.classList.add('showCart')
+       cartOverlay.classList.add("transparentBcg");
+       cartDOM.classList.add('showCart');
 
    }
 
+   hideCart() {
+       cartOverlay.classList.remove("transparentBcg");
+       cartDOM.classList.remove('showCart');
+   }
+
+    setupAPP() {
+        cart = Storage.getCart();
+        this.setCartValues(cart);
+        this.populate(cart);
+        cartBtn.addEventListener('click', this.showCart);;
+        closeCartBtn.addEventListener('click', this.hideCart)
+       
+
+    }
+
+    populate(cart) {
+        cart.forEach(item => this.addCartItem(item));
+    }
+
+    cartLogic() {
+        //clear cart button 
+        clearCart.addEventListener('click', () => {
+        this.clearCart();
+        });
+
+        //cart funtionality
+        cartContent.addEventListener('click', event => {
+            if(event.target.classList.contains('remove-item')){
+                let removeItem = event.target;
+                let id = removeItem.dataset.id;
+                
+                cartContent.removeChild(removeItem.parentElement.parentElement);
+                this.removeItem(id);
+                
+            } else if(event.target.classList.contains('fa-chevron-up')) {
+                let addAmount = event.target;
+               
+                let id = addAmount.dataset.id;
+                
+                let tempItem = cart.find(item => item.id === id);
+                tempItem.amount += 1;
+                Storage.saveCart(cart);
+                this.setCartValues(cart);
+                addAmount.nextElementSibling.innerText = tempItem.amount;
+            } else if(event.target.classList.contains('fa-chevron-down')) {
+                let addAmount = event.target;
+               
+                let id = addAmount.dataset.id;
+                
+                let tempItem = cart.find(item => item.id === id);
+                tempItem.amount -= 1;
+                Storage.saveCart(cart);
+                this.setCartValues(cart);
+                addAmount.previousElementSibling.innerText = tempItem.amount;
+            } 
+        })
+
+
+
+    }
+    
+    clearCart(){
+        let cartItems = cart.map(item => item.id);
+        cartItems.forEach(id => this.removeItem(id));
+        while(cartContent.children.length > 0){
+            cartContent.removeChild(cartContent.children[0])
+        }
+        this.hideCart();
+    }
+
+    removeItem(id){
+        cart = cart.filter(item => item.id !== id);
+        this.setCartValues(cart);
+        Storage.saveCart(cart);
+        let button = this.getSingleButton(id);
+        button.disabled = false;
+        button.innerHTML = `<i class="fas fa-shopping-cart"></i> add to cart`
+    }
+
+    getSingleButton(id){
+        return buttonsDOM.find(button => button.dataset.id === id)
+    }
+
+
+
+
+
+  
+
+
 }
-
-
-
-
-
-
 
 
 //local storage---------------------------------------
@@ -171,19 +255,26 @@ class Storage {
         localStorage.setItem('cart', JSON.stringify(cart))
     }
 
+    static getCart() {
+        return localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : [];
+    }
+
 }
 
 document.addEventListener("DOMContentLoaded", () => {
 const ui = new UI();
 const products = new Products();
 
+//setup application
+ui.setupAPP();;
 //get all products
 
 products.getProducts().then(products => {
     ui.displayProducts(products);
     Storage.saveProducts(products);
 }).then(()=> {
-ui.getCartButtons()
+ui.getCartButtons();
+ui.cartLogic();
 
 
 });
